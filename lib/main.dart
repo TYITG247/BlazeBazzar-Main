d5689ff399fefec3a4f7e96de2db92abe9665759
NirajSahu0412 <nirajsahu0412@gmail.com>
2024-01-21 23:08:03 +0530

    Section 26

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: CustomLightTheme(),
       // darkTheme: CustomDarkTheme(),
       // themeMode: ThemeMode.system,
-      home: MainScreen(),
+      home: LoginScreen(),
     );
   }
 }
