ba1db24b2218f4046fe98acecf0b39503ca8c43b
waghkomal <waghkomal2003@gmail.com>
2024-01-23 20:44:27 +0530

    changes in upload screen dart

--- lib/seller/views/screens/upload_screen.dart
+++ lib/seller/views/screens/upload_screen.dart
@@ -5,8 +5,36 @@ class UploadScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("UPLOAD SCREEN"),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.yellow.shade900,
+          elevation: 0,
+          bottom: const TabBar(tabs: [
+            Tab(
+              child: Text('General'),
+            ),
+
+            Tab(
+              child: Text('Shipping'),
+            ),
+            Tab(
+              child: Text('Attributes'),
+            ),
+            Tab(
+              child: Text('Images'),
+            ),
+          ]),
+        ),
+
+        body: const TabBarView(children: [
+          Center(child: Text('General'),),
+          Center(child: Text('Shipping'),),
+          Center(child: Text('Attributes'),),
+          Center(child: Text('Images'),),
+        ],),
+      ),
     );
   }
 }
