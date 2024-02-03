adbd381230a7adb77daeaa563a21140dc3698813
NirajSahu0412 <nirajsahu0412@gmail.com>
2024-02-03 19:58:42 +0530

    Section 58

--- lib/buyers/views/inner_screens/checkout_screen.dart
+++ lib/buyers/views/inner_screens/checkout_screen.dart
@@ -1,108 +1,181 @@
+import 'package:blazebazzar/buyers/views/main_screen.dart';
 import 'package:blazebazzar/config/app_ui.dart';
 import 'package:blazebazzar/providers/cart_provider.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
-class CheckoutScreen extends StatelessWidget {
+class CheckoutScreen extends StatefulWidget {
   const CheckoutScreen({super.key});
 
   @override
+  State<CheckoutScreen> createState() => _CheckoutScreenState();
+}
+
+class _CheckoutScreenState extends State<CheckoutScreen> {
+  @override
   Widget build(BuildContext context) {
     final CartProvider _cartProvider = Provider.of<CartProvider>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Checkout",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      body: ListView.builder(
-        shrinkWrap: true,
-        itemCount: _cartProvider.getCartItem.length,
-        itemBuilder: (context, index) {
-          final cartData = _cartProvider.getCartItem.values.toList()[index];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              child: SizedBox(
-                height: 200,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: 120,
-                        width: 120,
-                        child: Image.network(
-                          cartData.imageUrl[0],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+    final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+    CollectionReference users = FirebaseFirestore.instance.collection('buyers');
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(FirebaseAuth.instance.currentUser!.uid).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "Checkout",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            body: ListView.builder(
+              shrinkWrap: true,
+              itemCount: _cartProvider.getCartItem.length,
+              itemBuilder: (context, index) {
+                final cartData =
+                    _cartProvider.getCartItem.values.toList()[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: SizedBox(
+                      height: 200,
+                      child: Row(
                         children: [
-                          Text(
-                            cartData.productName,
-                            style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 5,
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              height: 120,
+                              width: 120,
+                              child: Image.network(
+                                cartData.imageUrl[0],
+                              ),
                             ),
                           ),
-                          Text(
-                            "₹" + cartData.price.toStringAsFixed(2),
-                            style: TextStyle(
-                              fontSize: 22,
-                              color: FlexColor.mandyRedLightPrimary,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 4,
-                            ),
-                          ),
-                          OutlinedButton(
-                            onPressed: null,
-                            child: Text(
-                              cartData.productSize,
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  cartData.productName,
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 5,
+                                  ),
+                                ),
+                                Text(
+                                  "₹" + cartData.price.toStringAsFixed(2),
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    color: FlexColor.mandyRedLightPrimary,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 4,
+                                  ),
+                                ),
+                                OutlinedButton(
+                                  onPressed: null,
+                                  child: Text(
+                                    cartData.productSize,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ],
+                  ),
+                );
+              },
+            ),
+            bottomSheet: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
+                onTap: () {
+                  _cartProvider.getCartItem.forEach(
+                    (key, item) {
+                      EasyLoading.show(status: "Placing Order");
+                      final orderId = Uuid().v4();
+                      _firestore.collection('orders').doc(orderId).set(
+                        {
+                          'orderId': orderId,
+                          'vendorId': item.sellerId,
+                          'email': data['email'],
+                          'phone': data['phone'],
+                          'address': data['address'],
+                          'buyerID': data['buyerID'],
+                          'fullName': data['fullName'],
+                          'productName': item.productName,
+                          'productPrice': item.price,
+                          'productId': item.productId,
+                          'productImage': item.imageUrl,
+                          'quantity': item.quantity,
+                          'productSize': item.productSize,
+                          'scheduleDate': item.scheduleDate,
+                          'orderDate': DateTime.now(),
+                        },
+                      ).whenComplete(
+                        () {
+                          setState(() {
+                            _cartProvider.getCartItem.clear();
+                          });
+                          EasyLoading.dismiss();
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return MainScreen();
+                              },
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width - 100,
+                  decoration: BoxDecoration(
+                    color: FlexColor.mandyRedLightPrimary,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Place Order",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        letterSpacing: 4,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
           );
-        },
-      ),
-      bottomSheet: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: InkWell(
-          onTap: (){
-
-          },
-          child: Container(
-            height: 50,
-            width: MediaQuery.of(context).size.width - 100,
-            decoration: BoxDecoration(
-              color: FlexColor.mandyRedLightPrimary,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Center(
-              child: Text("Place Order", style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                letterSpacing: 4,
-              ),),
-            ),
-          ),
-        ),
-      ),
+        }
+        return Text("loading");
+      },
     );
   }
 }
